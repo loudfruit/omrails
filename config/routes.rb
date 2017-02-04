fb4b4c830c234cda78185d4e8c9358d495472720
Matt Rosinski <mattrosinski@gmail.com>
2017-02-04 12:11:46 +1100

    Locked down user tweets to force sign in for edits

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'tweets'  => 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
